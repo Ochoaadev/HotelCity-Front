4beeec79d7f58ebf8f1ce6e77addb9474cafeb00
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-11-15 01:36:21 -0400

    Cambio en routes para agregar las rutas del Nav

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Form_reservas from "./routes/Formulario_Reservas";
+import About from "./components/AboutUs";
+import Service from "./components/services";
+import Advanced from './components/Benf'
 import App from "./App";
 
 export const router = createBrowserRouter([
@@ -11,5 +14,17 @@ export const router = createBrowserRouter([
     {
         path: '/formulario_reservas',
         element: <Form_reservas/>
+    },
+    {
+        path: '/AboutUs',
+        element: <About/>
+    },
+    {
+        path: '/services',
+        element: <Service/>
+    },
+    {
+        path: '/Benf',
+        element: <Advanced/>
     }
 ])
