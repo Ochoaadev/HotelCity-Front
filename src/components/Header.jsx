8a8cb7722bf467cc0ca146d3951e61a285199d12
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-11-15 01:37:39 -0400

    Se modifico el header para llamar a las rutas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,46 @@
 import { useNavigate } from 'react-router-dom';
 import React, {useState} from "react"
 import {RiMenu3Line, RiCloseFill } from 'react-icons/ri'
-import Form_reservas from "../routes/Formulario_Reservas";
 function Header(){
     const navigate = useNavigate();
     const handleOpenForm = () => {
-        // setIsFormOpen(true);
         navigate("/formulario_reservas");
-     };
+    };
+    const handleOpenHome = () => {
+        navigate("/");
+    };
+    const handleOpenAbutUs = () => {
+        navigate("/AboutUs");
+    };
+    const handleOpenservices = () => {
+        navigate("/services");
+    };
     const [showMenu, SetshowMenu] = useState(false)
     return(
         <>
       
         <header className="bg-AzulO flex items-center justify-between xl:justify-start w-full py-4 px-8 h-[14vh] z-50">
             <div className="xl:w-1/6 text-center -mt-4">
-                <a href="#" className="text-2xl font-bold p-1" >
-                    <img src="../src/assets/Logo.png" className="w-30 h-28 mt-5 ml-20" />
-                </a>
+                <button className="text-2xl font-bold p-1" onClick={handleOpenHome}>
+                    <img src="../src/assets/Logo.png" className="w-30 h-28 mt-3 ml-20" />
+                </button>
             </div>
             <nav className={`fixed bg-AzulO w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"}
              top-0 xl:static flex-1 flex flex-col xl:flex-row items-center gap-10 justify-center transition-all duration-500 z-50`}
-             >
-                <a href="#home" className="font-bold text-blanco">
-                    Home
-                </a>
-                <a href="#aboutUs" className="font-bold text-blanco">
-                    About Us
-                </a>
-                <a href="#advantages" className="font-bold text-blanco">
-                    Advantages
-                </a>
-                <a href="#services" className="font-bold text-blanco">
-                    Services
-                </a>
+            >
+            <button className="font-bold text-blanco" onClick={handleOpenHome}>
+                Inicio
+            </button>
+            <button className="font-bold text-blanco" onClick={handleOpenAbutUs}>Sobre Nosotros</button>
+
+
+<button className="font-bold text-blanco" onClick={handleOpenservices}>Servicios</button>
+<button className="font-bold text-blanco" onClick={handleOpenForm}>Reservar</button>
             </nav>
             <button onClick={()=>SetshowMenu(!showMenu)} className="xl:hidden text-2xl p-2"
             >
                 {showMenu ? <RiCloseFill/>:<RiMenu3Line/> }
             </button>
-            <button onClick={handleOpenForm}>Open</button>
         </header>
         
         </>
