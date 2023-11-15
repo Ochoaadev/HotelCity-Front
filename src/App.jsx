c74e9d296eecfb9cb7a9c69788aa9dc6ed0fcc4b
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-11-15 01:36:55 -0400

    Se modifico la pagina de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import Header from "./components/Header"
 import Info from './components/Info'
-import About from './components/AboutUs'
 import Advanced from './components/Benf'
 import Footer from './components/Footer'
 import Carrusel from './components/carrusel'
-import Service from './components/services'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Form_reservas from "./routes/Formulario_Reservas";
 function App() {
-
-
   return (
     <>
     <Header/>
     <Info/>
     <Carrusel/>
-    <About/>
     <Advanced/>
-    <Service/>
     <Footer/>
     </>
   )
 }
-
 export default App
