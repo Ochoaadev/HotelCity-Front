4a0a14285bdfd28feff9b46df0bd9bc04a8bdc5a
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-11-14 23:12:19 -0400

    Actualizando App para hacer uso de routes
    Asi poder redirigir al formulario de reservas

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Advanced from './components/Benf'
 import Footer from './components/Footer'
 import Carrusel from './components/carrusel'
 import Service from './components/services'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Form_reservas from "./routes/Formulario_Reservas";
 function App() {
 
 
