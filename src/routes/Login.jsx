3fa4122db25bd9df1cfe19c29486e3ae7af1bc7a
Ochoaadev <ochoarondonaa@uvm.edu.ve>
2023-11-19 12:57:33 -0400

    Implementando mejoras

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthProvider";
+import { useItemsContext, useUpItemsContext } from "../contexts/UpProvider";
 
 const Login = () => {
+
+    const { setMessage, setStatus, handleOpenMessage } = useItemsContext();
+
+    //Actualización de estados de Sección
+    const update = useUpItemsContext();
+    const navigate = useNavigate();
+    const { signin, isAuthenticated } = useAuth();
+  
+    //Comprobación de la existencia de un token
+    useEffect(() => {
+      const token = sessionStorage.getItem("token");
+      if (token != "" && isAuthenticated) {
+        navigate("/Info");
+      }
+    }, [isAuthenticated]);
 
   // Estableciendo los estados de los datos
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [message, setMessage] = useState();
+
   
   // Inputs sin contenidos
   const focusOnFirstEmptyInput = () => {
@@ -32,36 +48,62 @@ const Login = () => {
 
   //Función de Inicio de Sección
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    // e.preventDefault();
    
-    const alert = await focusOnFirstEmptyInput();
-    if (alert === true) {
-       return;
-    }
+    // const alert = await focusOnFirstEmptyInput();
+    // if (alert === true) {
+    //    return;
+    // }
    
-    const data_login = {
-       username,
-       password,
-    };
+    // const data_login = {
+    //    username,
+    //    password,
+    // };
    
-    //Solicitud al backend
-    const response = await fetch(import.meta.env.VITE_URL_LOGIN, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data_login),
-      });
+    // //Solicitud al backend
+    // const response = await fetch(import.meta.env.VITE_URL_LOGIN, {
+    //     method: "POST",
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data_login),
+    //   });
   
-      const data = await response.json();
-            console.log(data.message);
-            setMessage(data.message); // Actualiza el estado del mensaje
+    //   const data = await response.json();
+    //         console.log(data.message);
+    //         setMessage(data.message); // Actualiza el estado del mensaje
   
-            console.log(data.status);
+    //         console.log(data.status);
+
+    const Loguear = async (e) => {
+        e.preventDefault();
+        const alert = await focusOnFirstEmptyInput();
+        if (alert === true) {
+          return;
+        }
+    
+        const data_login = {
+          username,
+          password,
+        };
+    
+        //Solicitud al backend
+        const response = await signin(data_login);
+    
+        //Respuestas de la solicitud
+        if (response.data.status != 200) {
+          await setMessage(response.data.message);
+          await setStatus(response.data.status);
+          await handleOpenMessage();
+        }
+        navigate(response.ruta);
+      };
+
     }
 
  return (
     <>
+    <Message />
     <Header/>
     <div className="w-full max-w-5xl flex justify-center items-center m-auto mt-20 mb-28">
       <div className="bg-crema shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -96,8 +138,15 @@ const Login = () => {
             />
           </div>
           <div className="flex items-center justify-between">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-10" type="submit">
-              Iniciar Sesión
+          <button
+              type="submit"
+              onClick={(e) => {
+                Loguear(e);
+                update(true);
+              }}
+              className="px-2 flex items-center text-center bg-dark-tangerine dark:bg-gray/50 dark:hover:text-dark-tangerine dark:hover:bg-gray dark:border-woodsmoke justify-center h-10 rounded-lg hover:bg-pizazz focus:bg-blaze-orange dark:focus:bg-woodsmoke border-b-4 border-blaze-orange w-24 ssm:h-10 text-white font-bold"
+            >
+              Entrar
             </button>
             <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" to="/Registro">
                   Registrarse
