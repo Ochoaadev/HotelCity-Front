e1124242768e83d2509f8897486483dbf924d9d8
Ochoaadev <ochoarondonaa@uvm.edu.ve>
2023-11-20 00:29:16 -0400

    Implementando mejoras

--- src/routes/Room.jsx
+++ src/routes/Room.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Agg from '../components/Modal/AggRoom'
+import Edit from '../components/Modal/Edit_Room'
 
 const Habitaciones = () => {
   const [habitaciones, setHabitaciones] = useState([]);
@@ -28,6 +29,7 @@ const Habitaciones = () => {
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold my-6">Tipos de habitaciones disponibles</h1>
         <Agg />
+        <Edit/>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {habitaciones.map(habitacion => (
             <div key={habitacion.id} className="bg-gray-100 p-4 rounded-md">
