bcf9564a38478f797a88c8bc79546c58772dc849
yetzeniam7 <mendozaaya@uvm.edu.ve>
2023-11-19 22:16:05 -0400

    Se agrego edit reservas

--- src/components/Modal/Edit_Reserva.jsx
+++ src/components/Modal/Edit_Reserva.jsx
@@ -1,3 +1,4 @@
+import Message from "./Message";
 import { Modal, Button, ModalHeader, ModalBody, ModalFooter  } from "./Modal";
 import { useItemsContext } from "../../contexts/UpProvider";
 import { useEffect, useState } from "react";
@@ -9,24 +10,41 @@ function Edit_Reserva() {
     reservaIdToEdit
  } = useItemsContext();
    const { setMessage, setStatus, handleOpenMessage } = useItemsContext();
-  
 
+  const [inputmotivo, setinputmotivo] = useState();
+
+  const handlemotivo = (event) => {
+    setinputmotivo(event.target.value);
+ }
   const focusOnFirstEmptyInput = () => {
   
    return false;
  };
  const Editar = async (e) => {
   e.preventDefault(); // Evita que la página se recargue
-  const status = document.getElementById('status').value;
-  console.log(status);
-  console.log(reservaIdToEdit);
-  console.log("test");
+  const Status = document.getElementById('status').value;
+  const Data_Json = {
+     "Status":  Status,
+     "Motivo":  inputmotivo
+  };
+  const url = `http://localhost:4000/Edit_Reservas/${reservaIdToEdit._id}`;
+  const response = await fetch(url, {
+     method: "PUT",
+     headers: {
+        'Content-Type': 'application/json'
+     },
+     body: JSON.stringify(Data_Json),
+  });
+  const data = await response.json();
+  await setMessage(data.message);
+  await setStatus(data.status);
+  await handleOpenMessage();
+  handleOpenEdit_reserva(); // Cierra el modal después de guardar
 };
-
   
    return (
       <>
-      <>
+        <Message />
         {/* Modal */}
         <Modal open={openEdit_reserva} handleOpen={handleOpenEdit_reserva}>
           {/* Cabecera del modal */}
@@ -48,6 +66,14 @@ function Edit_Reserva() {
                  <option value="Rechazado">Rechazado</option>
                </select>
               </div>
+              <div className="mb-4 w-full">
+                <textarea name="motivo" id="" cols="30" rows="10"
+                className="border border-black"
+                    placeholder="Escribe el motivo de rechazo o aprobacion para enviar en el email"
+                  value={inputmotivo}
+                  onChange={handlemotivo}
+                ></textarea>
+              </div>
            
               
               
@@ -55,7 +81,7 @@ function Edit_Reserva() {
           </ModalBody>
           {/* Footer del Nodal */}
           <ModalFooter>
-            <Button open={openEdit_reserva}  handleOpen={handleOpenEdit_reserva}>Cancelar</Button>
+          <Button onClick={handleOpenEdit_reserva}>Cancelar</Button>
             <button
               className="flex items-center text-center bg-dark-tangerine dark:bg-gray/50 dark:hover:text-dark-tangerine dark:hover:bg-gray dark:border-woodsmoke justify-center h-10 px-4 mx-2 rounded-lg hover:bg-pizazz focus:bg-blaze-orange dark:focus:bg-woodsmoke border-b-4 border-blaze-orange ssm:w-20 ssm:h-8 ssm:px-0 ssm:my-1 text-white font-bold"
               onClick={(e) => {
@@ -66,7 +92,7 @@ function Edit_Reserva() {
             </button>
           </ModalFooter>
         </Modal>
-      </>
+    
     </>
   );
 }
